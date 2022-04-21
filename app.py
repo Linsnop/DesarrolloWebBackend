98d6b581ec319b70790e861d8ac1e2392c599386
Linsnop <linsnop@hotmail.com>
2022-04-21 18:51:55 -0500

    Test Usuarios

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 
+from http import client
+from telnetlib import TLS
 from flask import Flask, redirect, render_template, request, session, url_for
 import datetime
-
+import pymongo
 
 # FlASK
 #############################################################
@@ -10,6 +12,18 @@ app.permanent_session_lifetime = datetime.timedelta(days=365)
 app.secret_key = "super secret key"
 
 #############################################################
+
+# MONGODB
+#############################################################
+mongodb_key = "mongodb+srv://desarrollowebuser:desarrollowebpassword@cluster0.dfh7g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+client = pymongo.MongoClient(
+    mongodb_key, tls=True, tlsAllowInvalidCertificates=True)
+db = client.Escuela
+cuentas = db.alumno
+#############################################################
+
+
+
 
 @app.route('/')
 def home():
@@ -42,3 +56,12 @@ def logout():
     if "email" in session:
         session.clear()
         return redirect(url_for("home"))
+
+
+@app.route("/usuarios")
+def usuarios():
+    cursor = cuentas.find({})
+    users = []
+    for doc in cursor:
+        users.append(doc)
+    return render_template("/usuarios.html", data=users)
